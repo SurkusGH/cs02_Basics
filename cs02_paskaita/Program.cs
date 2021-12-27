aa8cc0a3e96fe2dd219a36c07a212e7d3aaebd35
Surkus <v.surkus@me.com>
2021-12-27 21:00:31 +0200

    Update Program.cs

--- cs02_paskaita/Program.cs
+++ cs02_paskaita/Program.cs
@@ -6,19 +6,39 @@ namespace cs02_paskaita
     {
         static void Main(string[] args)
         {
+            #region Teorija
+
             Console.WriteLine("cs02_paskaita!");
             // Kintamasis (variable) - tai programos elementas turintis vardą,
             // galintis kisti ir įgyti skirtingas reikšmes.
             int number = 5;
             string name = "Vilmasntas";
+            Console.WriteLine("Vardo ilgis:");
             Console.WriteLine(name.Length);
             number.CompareTo(number);
 
-            //asignment1();
-            asignment2();
-            asignment3();
+            //const int FIRST_NUMBER = 5; // <-- constantos nėra dažnai naudojamos
 
-            const int FIRST_NUMBER = 5;
+            // Operatoriai + - * / % ++ --
+            // Loginiai && || ! ==
+            // Relational == != > < >= <=
+
+            //int x = 5;
+
+            //bool averageGrade = x >= 5 ? "Number is bigger or equals to 5" : "Number is too small" ; // <-- Ternerio if'as
+
+            #endregion
+
+            //asignment1();
+            //asignment2();
+            //asignment3();
+            //asignment4();
+            //asignment5();
+            //asignment6();
+            //asignment7();
+            asignment8();
+            asignment9();
+            //asignment10();
 
         }
 
@@ -77,7 +97,68 @@ namespace cs02_paskaita
 
         public static void asignment4()
         {
+            //Swap values between two variables
+            int a = 1;
+            int b = 2;
+            int c;
 
+            c = b; // b = 2, o a = 1; <-- c kintamajam priskiriu b kintamojo reikšmę;
+            b = a; // b = 2, o a = 2; <-- b kintamajam priskiriu a kintamojo reikšmę;
+            a = c; // b = 1, o a = 2; <-- a kintamajam priskiriu c kintamojo reikšmę;
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+
+        }
+        public static void asignment5()
+        {
+            Console.WriteLine("Input 2 numbers");           //<-- Consolė paduoda tekstą
+            double d = double.Parse(Console.ReadLine());    //<-- Čia mes sakome, kad įvestis yra double tipo kintamojo
+            double e = double.Parse(Console.ReadLine());    //<-- tas double.Parse nurodo kompiuteriui, kad jis defaultu priimamą stingą priimtų kaip double tipo kintamąjį;
+            bool isEqual = d == e;                      // <-- čia mes kuriame bool tipo kintmąjį ir sakome ar lygūs kintamiej d ir e, jeigu taip bool bus true, jeigu ne false
+            Console.WriteLine(isEqual);                 // išspausdiname bool reikšmę
+        }
+
+        public static void asignment6()
+        {
+            Console.WriteLine("Input 2 numbers");
+            double f = double.Parse(Console.ReadLine());
+            double g = double.Parse(Console.ReadLine());
+            Console.WriteLine(++f);                   // <-- mes sakome, prie kindamojo f pridedame 1;
+            Console.WriteLine(--g);                   // <-- mes sakome, prie kindamojo g atimame 1;
+        }
+
+        public static void asignment7()
+        {
+            double borderA = double.Parse(Console.ReadLine());
+            double borderB = double.Parse(Console.ReadLine());
+            Console.WriteLine($"Perimetras yra: {borderA*2 + borderB*2}"); // <-- čia yra elementari matematika
+            Console.WriteLine($"Plotas yra: {borderA * borderB}");
+        }
+
+        public static void asignment8()
+        {
+            Console.WriteLine("Enter a symbols, function checks if empty");
+            string check = Console.ReadLine();
+            var checkCheck = String.IsNullOrEmpty(check) ? "Empty" : "Not empty"; // <-- Čia mes naudojame metodą IsNullOrEmpty įvesčiai check;
+            Console.WriteLine(checkCheck);
+        }
+
+        public static void asignment9()
+        {
+            Console.WriteLine("Enter a number, function checks if less than 0");
+            double testTest = double.Parse(Console.ReadLine());
+            var lessThan = testTest < 0 ? "input is less than 0" : "input is bigger than 0";
+            Console.WriteLine(lessThan);
+        }
+
+        public static void asignment10()
+        {
+            Console.WriteLine($"Your name");
+            string name = Console.ReadLine();       // <-- Pavyzdžiui čia mes nenaudojame parse, nes default įvestis yra string'as
+            Console.WriteLine($"Your last name");
+            string lastName = Console.ReadLine();
+
+            Console.WriteLine($"{name}, {lastName}");
         }
     }
 }
